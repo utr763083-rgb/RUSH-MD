35738402d3ca7f69de98ec0b649bb2a423035e1e
utr763083-rgb <utr763083@gmail.com>
2025-09-19 18:16:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "VISPER-MD&GkAi2ZpS#Ha2jIplgAYRj12CWOnpWyTU6DF9uI04mSWWKzjndReQ",
+    SESSION_ID: process.env.SESSION_ID || "VISPER-MD&ug5j2KZJ#8zLbmXW0K5MdtCvjx1MgKCu2DOBtcjAt3x9dX-cYvK4",
     // add your Session Id make sure it starts with VISPER-MD&
 
     CINESUB_API_KEY: process.env.CINESUB_API_KEY || "e0795fcd-58a9-4c0e-994d-648f146077cb",
